587f12eaaf40d75ae53219e96a8c361abd06fab6
mr-jameson <elliot.jameson4@gmail.com>
2019-12-12 16:36:58 +1100

    full CRUD resource

--- routes/index.js
+++ routes/index.js
@@ -66,4 +66,49 @@ router.delete('/api/v1/list/:id', (req, res) => {
     });
 });
 
+router.put('/api/v1/list/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  let listFound;
+  let itemIndex;
+  db.map((list, index) => {
+    if (list.id === id) {
+      listFound = list;
+      itemIndex = index;
+    } 
+  });
+
+  if (!listFound) {
+    return res.status(404).send({
+      success: 'false',
+      message:  'list not found'
+    });
+  }
+
+  if (!req.body.item) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'item is required'
+    });
+  } else if (!req.body.notes) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'notes is required'
+    });
+  }
+
+  const updatedList = {
+    id: listFound.id,
+    item: req.body.item || listFound.item,
+    notes: req.body.notes || listFound.notes,
+  };
+
+  db.splice(itemIndex, 1, updatedList);
+
+  return res.status(201).send({
+    success: 'true',
+    message:  'list added successfully',
+    updatedList
+  });
+});
+
 module.exports = router;
